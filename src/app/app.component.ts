d78e161bf32bcb68a74636326df20371a1c8b418
gchoi <cinema4dr12@gmail.com>
2022-11-07 20:37:27 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {
+    Observable,
+    timer,
+    map,
+    finalize,
+    concatMap,
+    Subject
+} from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular-Rxjs-Queue';
+export class AppComponent implements OnInit {
+
+    title = 'Angular-Rxjs-Queue';
+
+    obsQueue = new Subject<Observable<any>>();
+    obsQueueCount = 1;
+
+    funcObsQueue = new Subject<() => Observable<any>>();
+    funcObsQueueCount = 1;
+
+    public ngOnInit() {
+        this.process();
+    }
+
+    public add() {
+        this.enqueue();
+    }
+
+    public addFuncObs() {
+        const currentCount = this.funcObsQueueCount;
+
+        console.log('[QUEUING]', currentCount)
+
+        const subject = timer(1000).pipe(map(x => currentCount));
+
+        this.funcObsQueue.next(() => {
+            console.log('executing func')
+            return subject;
+        })
+
+        this.funcObsQueueCount++;
+    }
+
+    private enqueue() {
+        const currentCount = this.obsQueueCount;
+
+        console.log('[QUEUING]', currentCount)
+        const subject = timer(1000).pipe(map(x => currentCount));
+        this.obsQueue.next(subject)
+
+        this.obsQueueCount++;
+    }
+
+    private process() {
+        console.log('PROCESSING QUEUE...')
+
+        this.obsQueue
+            .pipe(
+                finalize(() => console.log('stopped processing queue')),
+                concatMap(x => x))
+            .subscribe(x => {
+                console.log('[PROCESSED]', x)
+            });
+
+        this.funcObsQueue
+            .pipe(
+                finalize(() => console.log('stopped processing queue')),
+                concatMap(x => x()))
+            .subscribe(x => {
+                console.log('[PROCESSED]', x)
+            });
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HelloComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
